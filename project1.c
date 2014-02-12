43ec6a97120ee731968340fddba5a88e559e7a48
Yuan Xia <yuan.xia@yale.edu>
2014-02-11 22:48:26 -0500

    finish calculating u matrix

--- project1.c
+++ project1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 void printMatrix(double *a, int n) {
   printf("***************** Matrix %d x %d *********************\n", n, n);
   int i = 0;
   for (i = 0; i < n * n; ++i) {
-    printf("%20.15f ", a[i]);
+    printf("%8.5f ", a[i]);
     if ((i+1) % n == 0)
       printf("\n");
   }
@@ -39,37 +40,105 @@ double* multiply(double *a, double *b, int p, int q, int r) {
   return result;
 }
 
-void normalizeVector(double *a, int n) {
-	double sum = 0;
-	int i;
-	for (i = 0; i < n; i++)
-		sum += a[i] * a[i];
-	sum = sqrt(sum);
+void normalizeCol(double *a, int n, int col) {
+  double sum = 0;
+  int i;
+  for (i = 0; i < n; i++) {
+    sum += a[n*i+col] * a[n*i+col];
+  }
+  sum = sqrt(sum);
 
-	if (sum == 0)
-		return;
-	for (i = 0; i < n; i++)
-		a[i] /= sum;
-	return;
+  if (sum == 0)
+    return;
+  for (i = 0; i < n; i++)
+    a[n*i+col] /= sum;
+  return;
+}
+double dotProductForCols(double *a, int n, int col1, int col2) {
+  int i;
+  double result = 0.0;
+  for (i = 0; i < n; i++)
+    result += a[n*i+col1]*a[n*i+col2];
+  return result;
+}
+void updateFromPrevCols(double *u, int n, int col) {
+  int i, j;
+  double* tmp = (double *)malloc(n*sizeof(double));
+  for (i = 0; i < n; i++)
+    tmp[i] = 0.0;
+  
+  //sum of all prev dot products
+  for (i = 0; i < col; i++) {
+    double dot = dotProductForCols(u, n, col, i);
+    for (j = 0; j < n; j++) 
+      tmp[j] += dot * u[j*n+i];
+  }
+
+  //update current col
+  for (i = 0; i < n; i++)
+    u[i*n+col] -= tmp[i];
+  
+  return;
+}
+
+
+void updateFollowingCols(double *u, int n, int col) {
+  int i, j;
+  for (j = col; j < n; j++) {
+    double dot = dotProductForCols(u, n, j, col-1);
+    //update j-th col with (col-1)-th unit vector
+    for (i = 0; i < n; i++)
+      u[i*n+j] -= dot * u[i*n+col-1];
+  }
+
+  return;
 }
 
 void inv_double_gs(double *a, int n, double *u, double *b) {
-	return;
-}
-void problem1(int n) {
-	int i, j;
+  
+  int i, j;
+  for (i = 0; i < n; i++) {
+    updateFromPrevCols(u, n, i);
+    normalizeCol(u, n, i);
+    updateFollowingCols(u, n, i+1);
+  }
 
-	double *a = (double *)malloc(n*n*sizeof(double));
 
-	for (i = 0; i < n; i++)
-		for (j = 0; j < n; j++)
-			a[i*n+j] = (double)rand()/(double)RAND_MAX;
+  //printMatrix(a, n);
+  //printMatrix(u, n);
 
+  double* ut = (double*)malloc(n*n*sizeof(double));
+  memcpy(ut, u, n*n*sizeof(double));
+
+  //transpose(ut, n);
+  
+  printMatrix(multiply(u, ut, n, n, n), n);
+    
 
   
+  return;
+}
+void problem1(int n) {
+  int i, j;
+
+  double *a = (double *)malloc(n*n*sizeof(double));
+  double *u = (double *)malloc(n*n*sizeof(double));
+  double *b = (double *)malloc(n*n*sizeof(double));
+
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      a[i*n+j] = (double)rand()/(double)RAND_MAX;
+      //a[i*n+j] = rand() % 10;
+
+  //printMatrix(a, n);
+  
+  memcpy(u, a, n*n*sizeof(double));
+  
+  inv_double_gs(a, n, u, b);
+
 }
 
 int main() {
-	
-	return 0;
+  problem1(10);
+  return 0;
 }
